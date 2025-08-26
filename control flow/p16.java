c644bb54fc4cf1ec8d9f514c5c60182df3ab8550
Ayushagarwalll <itisayushagarwal@gmail.com>
2025-08-26 14:04:09 +0530

    new

---
+++ control flow/p16.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class p16
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n>0)
+        {
+            for(int i=1;i<=n;i++)
+            {
+                System.out.print(i);
+                if(i%2==0)
+                System.out.println( " even");
+                else
+                System.out.println( " odd");
+            }
+        }
+        else
+        System.out.println(" not a natural no");
+    }
+}
