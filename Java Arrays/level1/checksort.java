7b384d128c326c6e10caf5df8726ff482df237f5
Ayushagarwalll <itisayushagarwal@gmail.com>
2025-08-27 15:17:57 +0530

    array

--- Java Arrays/level1/checksort.java
+++ Java Arrays/level1/checksort.java
@@ -6,7 +6,7 @@ public class checksort
         Scanner sc=new Scanner(System.in);
         int r=2,c=2;
         int [][]arr=new int[r][c];
-        int i,j;
+        int i,j,tem=0;
         boolean b=true;
           for(i=0;i<r;i++){
             for(j=0;j<c;j++){
@@ -21,18 +21,18 @@ public class checksort
 
              for( i=0;i<r;i++)
              {
+            
             for( j=0;j<c-1;j++)
             {
-                if(arr[i][j]<arr[i][j+1])
-                b=true;
-                else
+                if(arr[i][j]>arr[i][j+1]){
                 b=false;
-                if(i<r-2)
                 break;
-            }if(arr[i][j]<arr[i+1][0])
-            b=true;
-            else
+            }}
+            if(i+1>r)
+                 break;
+            if(arr[i][j]>arr[i+1][0]){
             b=false;
-             }
+            break;
+             }}
              System.out.println(b);
     }}
